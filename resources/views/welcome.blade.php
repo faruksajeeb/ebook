b45642141c10890f215168534adc42cf73846187
Omar Faruk <ofsajeeb@gmail.com>
2023-03-13 07:47:03 +0600

    templating mastering: daynamic sidebar added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,15 +12,18 @@
     <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/ruang-admin.min.css') }}" rel="stylesheet">
-    
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    </head>
 
-   <body >
-    <div id="app">  
-           
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body id="page-top">
+    <div id="app">
+
     </div>
-    
+    <!-- Scroll to top -->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
     <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
